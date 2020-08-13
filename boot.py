cb11d8112a116e876c5efb1a760e83f753455d5a
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-08-13 20:46:52 +0200

    feat(boot.py): use ssid/password from settings.py

--- boot.py
+++ boot.py
@@ -3,17 +3,14 @@
  Author Shanmugathas Vigneswaran
 """
 import network
-
-SSID = 'raspi-webgui'
-PASSWORD = 'ChangeMe'
-
+from settings import WIFI_SSID, WIFI_PASSWORD
 
 def connect_access_point():
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect(SSID, PASSWORD)
+        sta_if.connect(WIFI_SSID, WIFI_PASSWORD)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
