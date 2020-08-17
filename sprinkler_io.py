d2a7c94d7fabdab0c3edabcc66517fc88abb0d39
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-08-17 20:43:48 +0200

    feat(sprinkler_io, sprinkler-firmware/sprinkler_io.py): add min/max, sensor value float to integer

--- sprinkler_io.py
+++ sprinkler_io.py
@@ -17,10 +17,20 @@ soil_moisture_map = fit(
 
 def read_sensors(tag):
     soil_moisture_raw_adc = soi_moisture_sensor.read()
+    soil_moisture = soil_moisture_map(soil_moisture_raw_adc)
+
+    # builtin round function seem not working
+    # https://forum.micropython.org/viewtopic.php?t=802
+    # soil_moisture = round(soil_moisture, 2)
+    soil_moisture = int(soil_moisture)
+    if soil_moisture < 0:
+        soil_moisture = 0
+    elif soil_moisture > 100:
+        soil_moisture = 100
     return {
         "tag": tag,
         "soil_moisture_raw_adc": soil_moisture_raw_adc,
-        "soil_moisture": int(soil_moisture_map(soil_moisture_raw_adc))
+        "soil_moisture": soil_moisture
     }
 
 
